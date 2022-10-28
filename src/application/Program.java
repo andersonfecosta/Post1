afa7d995442b7cd39aede1a7d04c1f43ec0fe386
andersonfecosta <116733467+andersonfecosta@users.noreply.github.com>
2022-10-28 17:05:17 -0300

    Update Program.java

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,7 +9,7 @@ import java.text.SimpleDateFormat;
 public class Program {
     public static void main(String[] args) throws ParseException {
 
-        SimpleDateFormat fmrt = new SimpleDateFormat("dd/MM/yyy HH:mm:ss");
+        SimpleDateFormat fmrt = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 
         Comment c1 = new Comment("Have a nice trip!");
         Comment c2 = new Comment("Wow thats awesome!");
